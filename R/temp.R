bfad9e2b7b8a9d0caad1ec3da4c71626d63140e2
Eduardo Lemos Júnior <elemosjr@gmail.com>
2021-03-23 21:14:16 -0300

    add grafico obitos mes

--- R/temp.R
+++ R/temp.R
@@ -1,9 +1,24 @@
 library(tidyverse)
 
+Sys.setlocale("LC_TIME", "pt_BR.UTF-8")
+
 dados <- read_csv("../data/maringa.csv")
 
-dados %>% 
+dados1 <- dados %>%                                                            
   separate(data, c("dia", "mes", "ano"), "/") %>%
   mutate(novos_obitos = obitos - lag(obitos, default = 0)) %>%
   group_by(ano, mes) %>%
-  summarize(novos_obitos = sum(novos_obitos))
+  summarize(novos_obitos = sum(novos_obitos)) %>% unite("data", mes:ano, sep = "/") %>% mutate(data = lubridate::dmy(paste0("1/", data))) 
+
+dados1 %>%
+  ggplot(aes(x = data, y = novos_obitos)) +
+  geom_col() +
+  geom_label(aes(label = novos_obitos), nudge_y = 5) +
+  labs(x = "Data (Mês)", y = "Novos Obitos",
+       title = "Obitos por COVID-19 em Maringá por mês",
+       caption = "Fonte: Prefeitura de Maringá") +
+  scale_x_date(breaks = dados1$data,
+               labels = format(dados1$data, format = "%b-%y")) +
+  theme(axis.text.x = element_text(angle = 10))
+
+ggsave("temp.png")
